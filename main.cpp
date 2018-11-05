f3096c13d4c0d313b3378783409ad25c0aa0fadc
Petr Wudi <petr.wudi@gmail.com>
2018-11-05 16:09:31 +0100

    Time measurement, change particle attrs and dynamic allocation (maybe unnecessary)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <ctime>
 
 #include "particle.cpp"
 
@@ -11,41 +12,44 @@
 
 using namespace std;
 
-vector<Particle> loadParticles(istream& input);
-vector<Vec3<double>> nbody(const vector<Particle>& particles);
-void moveParticles(vector<Particle>& particles, const vector<Vec3<double>>& forces);
-void printParticles(const vector<Particle>& particles, ostream& out);
+vector<Particle> * loadParticles(istream& input);
+vector<Vec3<double>> nbody(const vector<Particle> * particles);
+void moveParticles(vector<Particle> * particles, const vector<Vec3<double>>& forces);
+void printParticles(const vector<Particle> * particles, ostream& out);
 
 int main(int argc, char** argv) {
-  vector<Particle> particles = loadParticles(cin);
+  vector<Particle> * particles = loadParticles(cin);
+  clock_t start = clock();
   for(int i = 0; i < 500; ++i) {
     vector<Vec3<double>> forces = nbody(particles);
     moveParticles(particles, forces);
   }
+  clock_t end = clock();
+  cout << "Time: " << (end - start) << " ms" << endl;
   printParticles(particles, cout);
+  delete particles;
   return 0;
 }
 
-vector<Particle> loadParticles(istream& input) {
-  vector<Particle> particles;
+vector<Particle> * loadParticles(istream& input) {
+  vector<Particle> * particles = new vector<Particle>();
   double x;
   double y;
   double z;
   double mass;
   while(input >> x && input >> y && input >> z && input >> mass) {
-    particles.push_back(Particle(x, y, z, mass));
+    particles->push_back(Particle(x, y, z, mass));
   }
   return particles;
 }
 
-
-vector<Vec3<double>> nbody(const vector<Particle>& particles) {
+vector<Vec3<double>> nbody(const vector<Particle> * particles) {
   vector<Vec3<double>> forces;
-  for (auto it1 = particles.begin(); it1 < particles.end(); ++it1) {
+  for (auto it1 = particles->begin(); it1 < particles->end(); ++it1) {
     Vec3<double> res(0, 0, 0);
-    for (auto it2 = particles.begin(); it2 < particles.end(); ++it2) {
+    for (auto it2 = particles->begin(); it2 < particles->end(); ++it2) {
       if (it1 == it2) continue;
-      Vec3<double> diff = it1->direction - it2->direction;
+      Vec3<double> diff = it1->position - it2->position;
       double bottom = pow(diff.sqrSize() + SOFTENING_FACTOR_SQR, 1.5);
       double massTotal = GRAVITATION_CONSTANT * it1->mass * it2->mass;
       res += diff * massTotal / bottom;
@@ -55,18 +59,19 @@ vector<Vec3<double>> nbody(const vector<Particle>& particles) {
   return forces;
 }
 
-void moveParticles(vector<Particle>& particles, const vector<Vec3<double>>& forces) {
-  auto particleIt = particles.begin();
+void moveParticles(vector<Particle> * particles, const vector<Vec3<double>>& forces) {
+  auto particleIt = particles->begin();
   auto forceIt = forces.begin();
-  while(particleIt < particles.end() && forceIt < forces.end()) {
-    particleIt->direction += *forceIt;
+  while(particleIt < particles->end() && forceIt < forces.end()) {
+    // particleIt->position += *forceIt / particleIt->mass;
+    particleIt->position += *forceIt;
     ++particleIt;
     ++forceIt;
   }
 }
 
-void printParticles(const vector<Particle>& particles, ostream& out) {
-  for(auto it = particles.begin(); it < particles.end(); ++it) {
+void printParticles(const vector<Particle> * particles, ostream& out) {
+  for(auto it = particles->begin(); it < particles->end(); ++it) {
     cout << *it << endl;
   }
 }
--- particle.cpp
+++ particle.cpp
@@ -3,14 +3,15 @@
 class Particle {
     private:
     public:
-        Vec3<double> direction;
+        Vec3<double> position;
+        Vec3<double> velocity;
         double mass;
-        Particle() : direction(Vec3<double>()), mass(0) { };
-        Particle(double x, double y, double z, double mass) : direction(Vec3<double>(x, y, z)), mass(mass) { };
-        Particle(Vec3<double> direction, double mass) : direction(direction), mass(mass) { };
+        Particle() : position(Vec3<double>()), velocity(Vec3<double>()), mass(0) { };
+        Particle(double x, double y, double z, double mass) : position(Vec3<double>(x, y, z)), velocity(Vec3<double>(0, 0, 0)), mass(mass) { };
+        Particle(Vec3<double> position, double mass) : position(position), velocity(Vec3<double>(0, 0, 0)), mass(mass) { };
 
         friend std::ostream& operator<<( std::ostream &o, const Particle &particle) {
-            o << particle.direction << " " << particle.mass;
+            o << particle.position << " " << particle.mass;
             return o;
         }
 };
